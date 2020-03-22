881b79d161423d7f4a3b3674b3670bd7d83e196a
Anton Begehr <a.begehr@gmail.com>
2020-03-22 13:23:21 +0100

    remove aside

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,25 +13,6 @@
     </nav>
 
     <section class="main-content columns">
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">
-          General
-        </p>
-        <ul class="menu-list">
-          <li
-            v-for="(item, key) of items"
-            :key="key"
-          >
-            <nuxt-link
-              :to="item.to"
-              exact-active-class="is-active"
-            >
-              <b-icon :icon="item.icon" /> {{ item.title }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </aside>
-
       <div class="container column is-10">
         <nuxt />
       </div>
