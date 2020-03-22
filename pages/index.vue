cbd801e6215dd8c1d70e3ed982b7f8c223c0f70e
Anton Begehr <a.begehr@gmail.com>
2020-03-22 16:24:56 +0100

    fetchItems on mount

--- pages/index.vue
+++ pages/index.vue
@@ -17,32 +17,49 @@
 import Login from "~/components/Login";
 import Card from "~/components/Card";
 
+const questions = {
+  verunsicherter_buerger: "Ich bin verunsichert. Was nun?",
+  gastro_betrieb: "Ich habe einen Gastro Betrieb. Was nun?",
+  corona_diagnose: "Wo kann ich einen Test machen?"
+};
+
 export default {
   name: "HomePage",
-  data() {
-    return {
-      items: [
-        {
-          id: "1",
-          question: "Was bedeutet häusliche Quarantäne?",
-          answer: "HEy1"
-        },
-        {
-          id: "2",
-          question: "Woran erkenne ich Corona?",
-          answer: "HEy2"
-        },
-        {
-          id: "3",
-          question: "Wo kann ich einen Test machen?",
-          answer: "HEy3"
-        }
-      ]
-    };
-  },
   components: {
     Login,
     Card
+  },
+  data() {
+    return {
+      items: []
+    };
+  },
+  mounted: function() {
+    this.fetchItems();
+  },
+  methods: {
+    fetchItems: function() {
+      this.$axios.get("/answers").then(res => {
+        const items = {};
+        const answers = {};
+
+        res.data
+          .filter(item => item.channel.key === "test-website-1") //DEBUG filtering is debug only!
+          .forEach(item => {
+            answers[item.question_id] = item.answer_text;
+          });
+
+        Object.keys(questions).forEach(id => {
+          items[id] = {
+            id: id,
+            question: questions[id],
+            answer: answers[id] || ""
+          };
+        });
+
+        this.items = items;
+      });
+    }
   }
 };
 </script>
